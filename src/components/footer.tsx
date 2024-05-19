bda3bdb1891ece8c3dc407ef6a65bf19a45f830a
akxcix <git@akxcix.com>
2024-05-19 03:30:47 -0400

    change defaults to be specific to koerolabs

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,4 @@
-import { Flower, Github, Twitter } from "lucide-react";
+import { Github, Send, Twitter } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
@@ -7,20 +7,20 @@ export default function Footer() {
             <footer className="sticky flex h-16 items-center gap-4 border-t bg-background px-4 md:px-6">
             <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                     <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-                        <Flower className="h-6 w-6" />
-                        <span className="sr-only">Acme Inc</span>
+                        <Send className="h-6 w-6" />
+                        <span className="sr-only">Koero Labs</span>
                     </Link>
                 </nav>
                 <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
                     <div className="ml-auto flex-1 sm:flex-initial">
                         <div className="flex items-center gap-2 text-lg font-semibold md:text-base">
-                            <Link to="https://github.com/akxcix" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+                            <Link to="https://github.com/koerolabs" className="flex items-center gap-2 text-lg font-semibold md:text-base">
                                 <Github className="h-6 w-6" />
-                                <span className="sr-only">github/akxcix</span>
+                                <span className="sr-only">github/koerolabs</span>
                             </Link>
-                            <Link to="https://twitter.com/ak_xcix" className="flex items-center gap-2 text-lg font-semibold md:text-base">
+                            <Link to="https://twitter.com/koerolabs" className="flex items-center gap-2 text-lg font-semibold md:text-base">
                                 <Twitter className="h-6 w-6" />
-                                <span className="sr-only">twitter/ak_xcix</span>
+                                <span className="sr-only">twitter/koerolabs</span>
                             </Link>
                         </div>
                     </div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,5 @@
-import { Flower, Search } from "lucide-react";
+import { Send } from "lucide-react";
 import { Link } from "react-router-dom";
-import { Input } from "@/components/ui/input";
 import { ModeToggle } from "@/components/mode-toggle";
 
 const headerLinks = [
@@ -20,13 +19,16 @@ export default function Navbar() {
             <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
                 <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
                     <Link to="/" className="flex items-center gap-2 text-lg font-semibold md:text-base">
-                        <Flower className="h-6 w-6" />
-                        <span className="sr-only">Acme Inc</span>
+                        <Send className="h-6 w-6" />
+                        <span className="sr-only">Koero Labss</span>
                     </Link>
                     {headerLinks.map((link) => (<Link to={link.link}>{link.name}</Link>))}
                 </nav>
                 <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-                    <form className="ml-auto flex-1 sm:flex-initial">
+                    <div className="ml-auto flex-1 sm:flex-initial">
+
+                    </div>
+                    {/* <form className="ml-auto flex-1 sm:flex-initial">
                         <div className="relative">
                             <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                             <Input
@@ -35,7 +37,7 @@ export default function Navbar() {
                                 className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
                             />
                         </div>
-                    </form>
+                    </form> */}
                     <ModeToggle />
                 </div>
             </header>
